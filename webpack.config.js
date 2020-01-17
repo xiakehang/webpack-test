2003175cad8ec81d827707a792d1fd75f6af444a
pengxie <pengxie@fortinet.com>
2020-01-16 18:20:58 -0800

    dev server added

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: path.join(__dirname, 'src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true
   },
   module: {
     rules: [{
       test: /\.css/,
       use: ['style-loader', 'css-loader']
     }],
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject: true,
+    template: './public/index.ejs',
+  })]
 };
