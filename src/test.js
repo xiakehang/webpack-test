eec976a6164b193c47592e39eb3c910b48b363e8
xiakehang <aa.x@163.com>
2020-04-07 15:16:09 -0700

    test revert

--- src/test.js
+++ src/test.js
@@ -3,5 +3,5 @@ import React, { Component, createElement } from 'react';
 import stylemodule from './style.module.css'
 
 export default () => {
-    return <div className={stylemodule.label}>dddd</div>
+    return <div className={stylemodule.label}>ddd</div>
 }
