6ff35099d96f691071b00cafaba15d24ca00732b
xiakehang <aa.x@163.com>
2020-04-07 15:54:11 -0700

    foo

--- src/test.js
+++ src/test.js
@@ -2,8 +2,6 @@ import React, { Component, createElement } from 'react';
 
 import stylemodule from './style.module.css'
 
-
-
 export default () => {
     return <div className={stylemodule.label}>ddd</div>
     return 333;
